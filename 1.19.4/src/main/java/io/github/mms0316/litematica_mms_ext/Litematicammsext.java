9068addcc14c96537648ac887c7deb32fbc55323
mms <lurkingdeeply@gmail.com>
2023-11-28 21:23:52 -0300

    Added 1.19.2 and 1.19.4 versions
    - If someone knows how to reduce all this duplication please let me know

---
+++ 1.19.4/src/main/java/io/github/mms0316/litematica_mms_ext/Litematicammsext.java
@@ -0,0 +1,22 @@
+package io.github.mms0316.litematica_mms_ext;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Litematicammsext implements ClientModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("litematica-mms-ext");
+
+	@Override
+	public void onInitializeClient() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Litematica mms extensions loaded");
+	}
+}
